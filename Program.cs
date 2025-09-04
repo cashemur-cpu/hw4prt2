30d33781de8d04b21842b14a5cf4d2434507748f
Maksym B <cashemur@gmail.com>
2025-09-04 15:49:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace HW4prt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first operand");
+            string firstInput = Console.ReadLine();
+            Console.WriteLine("Enter second operand");
+            string secondInput = Console.ReadLine();
+            double firstOperand = 0;
+            double secondOperand = 0;
+            try
+            {
+                firstOperand = double.Parse(firstInput.Replace(".", ","));
+                secondOperand = double.Parse(secondInput.Replace(".", ","));
+                Console.WriteLine("Operands has been converted succesfully");
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Enter a numberic values");
+            }
+            Console.WriteLine("Select an operation \"+\" / \"-\" / \"*\" / \"/\"");
+            string operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"Result: {firstOperand + secondOperand}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Result: {firstOperand-secondOperand}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Result: {firstOperand*secondOperand}");
+                    break;
+                case "/":
+                    if (secondOperand != 0)
+                    {
+                        Console.WriteLine($"Result: {firstOperand/secondOperand}");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot devide by zero");
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Select an operation from suggested");
+                    break ;
+            }   
+        }
+    }
+}
